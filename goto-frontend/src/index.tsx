1c3a2daf1e05be8da61c5618d85a70b6a3c207c6
Kay A F <afkay.dev@icloud.com>
2024-03-15 03:42:43 +0300

    Update app id in html. Remove strict mode

--- goto-frontend/src/index.tsx
+++ goto-frontend/src/index.tsx
@@ -1,12 +1,7 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import { App } from './app';
 
 import './index.scss';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-);
+ReactDOM.createRoot(document.getElementById('app')!).render(<App />);
