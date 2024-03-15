d179122b3a5735b81791830353ec4617526d15b9
Kay A F <afkay.dev@icloud.com>
2024-03-15 04:34:20 +0300

    Update app header with app links

--- goto-frontend/src/app/app-header/app-header.component.tsx
+++ goto-frontend/src/app/app-header/app-header.component.tsx
@@ -1,13 +1,13 @@
-import { Link } from 'wouter';
-
 import styles from './app-header.module.scss';
+
+import { AppLink } from '../../shared/ui-kit';
 
 export const AppHeader: React.FC = () => {
   return (
     <div className={styles.Header}>
-      <Link to="/">Home</Link>
-      <Link to="/hello">Hello</Link>
-      <Link to="/make-link">Make link</Link>
+      <AppLink to="/">Home</AppLink>
+      <AppLink to="/hello">Hello</AppLink>
+      <AppLink to="/make-link">Make link</AppLink>
     </div>
   );
 };
